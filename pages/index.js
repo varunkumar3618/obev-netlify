fcbd2c2f23310fa40b9c9376b1b9884541d60a54
Varun Vijay <varunkumar3618@gmail.com>
2022-11-04 18:10:10 -0700

    Remove footer and change text.

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,17 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Oxford Book of English Verse</title>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Oxford Book of English Verse" />
       </main>
 
-      <Footer />
     </div>
   )
 }
